90282bea3cabbd8cf1a2db251227d6cf64bf41c4
Corbin Uselton <corbinu@openswimsoftware.com>
2014-07-12 13:42:50 -0700

    Add about page part 3

---
+++ ember/tests/integration/about-page-test.js
@@ -0,0 +1,21 @@
+import startApp from 'bostonember/tests/helpers/start-app';
+import Ember from 'ember';
+
+var App;
+
+module('Integration - About Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should navigate to the About page', function() {
+  visit('/').then(function() {
+    click("a:contains('About')").then(function() {
+      equal(find('h3').text(), 'About');
+    });
+  });
+});
