6987138ac5828e630eafd5eacb52e5c5feec9536
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-09 13:47:04 -0700

    Added a commit command to the store

--- ember/app/routes/speakers/edit.js
+++ ember/app/routes/speakers/edit.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
             var my_model = this.controller.get('model');
             console.log('***** my_model ' + my_model.get('name'));
             my_model.save();
+            this.get('store').commit();
 //            var my_speaker = this.model;
 //            console.log('***** my_speaker: "' + my_speaker + '"');
 //            var name = this.controller.get('name');
