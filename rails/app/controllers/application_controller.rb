ede57ca08a7b8d83b0fbace4e56a1feeffb37276
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-10 09:28:17 -0700

    Fixed CSRF exception

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  # Here's a solution from: https://coderwall.com/p/8z7z3a
+  # Here's a solution from oldfartdeveloper's comment
+  # on https://coderwall.com/p/8z7z3a
   protect_from_forgery with: :null_session,
-      if: Proc.new { |c| c.request.format == 'application/json' }
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
 end
