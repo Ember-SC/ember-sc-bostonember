b1446ff06ad5bb2dba052957be5bdbf637958263
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-10 20:07:55 -0700

    Added route for speaker.create

--- ember/app/routes/speakers/index.js
+++ ember/app/routes/speakers/index.js
@@ -1,5 +1,13 @@
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('speaker');
+  },
+
+  actions: {
+      create: function() {
+          var model = this.store.createRecord('speaker', function () {
+              name: ''
+          });
+      }
   }
 });
