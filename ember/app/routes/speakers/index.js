09bcd84a67ce885ddd30c7fc6eda92512663fd4c
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-10 20:25:42 -0700

    Removed unnecessary action from speakers.index route

--- ember/app/routes/speakers/index.js
+++ ember/app/routes/speakers/index.js
@@ -1,13 +1,5 @@
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('speaker');
-  },
-
-  actions: {
-      create: function() {
-          var model = this.store.createRecord('speaker', function () {
-              name: ''
-          });
-      }
   }
 });
