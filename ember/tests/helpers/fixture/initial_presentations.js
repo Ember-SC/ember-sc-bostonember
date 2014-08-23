f74258495017398b459ac253e4d9510ffa27a9af
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-23 15:27:07 -0700

    Now initial presentations are defined in their own file

---
+++ ember/tests/helpers/fixture/initial_presentations.js
@@ -0,0 +1,10 @@
+export default function initialPresentations() {
+    return [
+        { id: 1, title: "What's up with Docs?", speaker_id: 1 },
+        { id: 2, title: "Of course, you know, this means war.", speaker_id: 1 },
+        { id: 3, title: "Getting the most from the Acme categlog.", speaker_id: 2 },
+        { id: 4, title: "Shaaaad up!", speaker_id: 3 },
+        { id: 5, title: "Ah hates rabbits.", speaker_id: 3 },
+        { id: 6, title: "The Great horni-todes", speaker_id: 3 }
+    ];
+}
--- ember/tests/integration/speakers-page-test.js
+++ ember/tests/integration/speakers-page-test.js
@@ -1,6 +1,7 @@
 import startApp from 'bostonember/tests/helpers/start-app';
 import Ember from 'ember';
 import initialSpeakers from 'bostonember/tests/helpers/fixture/initial_speakers';
+import initialPresentations from 'bostonember/tests/helpers/fixture/initial_presentations';
 
 var App, server;
 
@@ -8,15 +9,7 @@ module('Integration - Speaker Page', {
     setup: function () {
         App = startApp();
         var speakers = initialSpeakers();
-
-        var presentations = [
-            { id: 1, title: "What's up with Docs?", speaker_id: 1 },
-            { id: 2, title: "Of course, you know, this means war.", speaker_id: 1 },
-            { id: 3, title: "Getting the most from the Acme categlog.", speaker_id: 2 },
-            { id: 4, title: "Shaaaad up!", speaker_id: 3 },
-            { id: 5, title: "Ah hates rabbits.", speaker_id: 3 },
-            { id: 6, title: "The Great horni-todes", speaker_id: 3 }
-        ];
+        var presentations = initialPresentations();
 
         server = new Pretender(function () {
             this.get('/api/speakers', function (request) {
