7b9fac8f7c0a9cf6f8648f5c81f82415bd60a40a
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-23 10:05:11 -0700

    Almost there on delete

--- ember/app/controllers/speakers/show.js
+++ ember/app/controllers/speakers/show.js
@@ -1,18 +1,18 @@
-export default Ember.Route.extend({
+export default Ember.ObjectController.extend({
 
     actions: {
-        submit: function() {
+        submit: function () {
             var my_model = this.controller.get('model');
             my_model.save();
             this.transitionTo('speakers.show', my_model);
         },
 
-        delete: function() {
-            var my_controller = this.controller;
-            my_controller.get('model').destroyRecord().then(function(){
+        delete: function () {
+            var my_controller = this;
+            my_controller.get('model').destroyRecord().then(function () {
                 console.log('********** Destroy Success! ***********');
                 my_controller.transitionToRoute('speakers.index');
-            }, function(reason) {
+            }, function (reason) {
                 console.log(reason);
             });
         }
