0b67198cc4e34b60f5a7a99868cec9345027a837
Scott Smith <scottnelsonsmith@gmail.com>
2014-08-25 00:00:01 -0700

    Can edit presentation title and dispatch to speaker show page

--- rails/app/controllers/api/presentations_controller.rb
+++ rails/app/controllers/api/presentations_controller.rb
@@ -8,4 +8,11 @@ class Api::PresentationsController < ApplicationController
     render json: Presentation.find(params[:id])
   end
 
+  def update
+    presentation = Presentation.find(params[:id])
+    presentation.title = params[:presentation]['title']
+    presentation.save!
+    render json: presentation
+  end
+
 end
