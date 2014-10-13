e104f6eb55e04b2d0b0f6d391c3273047b8d14e7
Scott Smith <scottnelsonsmith@gmail.com>
2014-10-12 21:33:32 -0700

    Readded broccoli configuration info for pretender

--- embersc/Brocfile.js
+++ embersc/Brocfile.js
@@ -17,8 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-//app.import('bower_components/route-recognizer/dist/route-recognizer.js');
-//app.import('bower_components/FakeXMLHttpRequest/fake_xml_http_request.js');
+app.import('bower_components/route-recognizer/dist/route-recognizer.js');
+app.import('bower_components/FakeXMLHttpRequest/fake_xml_http_request.js');
 //app.import('bower_components/pretender/pretender.js');
 
 module.exports = app.toTree();
